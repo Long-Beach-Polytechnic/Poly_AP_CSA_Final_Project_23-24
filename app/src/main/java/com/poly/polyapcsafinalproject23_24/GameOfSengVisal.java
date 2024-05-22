e3d9654c733020ff85d3c28da0a8cbbf24bbe469
Visal Seng <201194505@lbschools.net>
2024-05-22 09:54:56 -0700

    Project Setup

---
+++ app/src/main/java/com/poly/polyapcsafinalproject23_24/GameOfSengVisal.java
@@ -0,0 +1,369 @@
+package com.poly.polyapcsafinalproject23_24;
+
+import com.poly.polyapcsafinalproject23_24.GameActivity;
+
+import java.util.Scanner;
+
+public class GameOfSengVisal extends GameActivity {
+    /**
+     *fat body type preset
+     */
+    private Man bodyType1;
+    /**
+     *skinny body type preset
+     */
+    private Man bodyType2;
+    /**
+     *average body type preset
+     */
+    private Man bodyType3;
+    /**
+     *choosebody(); must be run before use
+     *tracks the current body type chosen
+     */
+    private Man currentBodyType; //tracks which body type is currently being used
+
+
+    /**
+     * Method that runs the game
+     */
+    public void run() {
+        welcomeScreen();
+        initialize();
+        chooseBody();
+        decision();
+        endScreen();
+
+
+
+
+    }
+
+    /**
+     *method for the pre-game screen
+     */
+    private void welcomeScreen() {
+        String text = """
+
+         ____ _____ _____   _____ ___ _____ _
+        / ___| ____|_   _| |  ___|_ _|_   _| |
+       | |  _|  _|   | |   | |_   | |  | | | |
+       | |_| | |___  | |   |  _|  | |  | | |_|
+       | ____|_____| |_|   |_|   |___| |_| (_)
+
+
+      """;
+        System.out.println(text);
+        System.out.println("Welcome to the body improvement simulator!");
+        System.out.println("\nYou can choose between 3 body types: ");
+        System.out.println("There some exercises you can perform, your maxes are recorded and you can increase them by pushing your limits bit by bit");
+        System.out.println("Diet is important too! There are multiple foods you can eat to manipulate your body's appearance!");
+        System.out.println("\nGood luck!");
+        Util.pauseConsole();
+        Util.clearConsole();
+    }
+
+    /**
+     *initialization of body types and the character name
+     */
+    private void initialize() {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter your name: ");
+        String characterName = scan.nextLine();
+
+
+        //initialize the instance variables
+
+        this.bodyType1 = new Man(characterName, 200.0, 30.0, 70.0, 150, 170, 1, 0);
+        this.bodyType2 = new Man(characterName, 100, 10.0, 90.0, 90, 100, 3, 0);
+        this.bodyType3 = new Man(characterName, 140, 15.0, 85.0, 120, 140, 2, 0);
+
+
+    }
+
+    /**
+     *decision by the user to choose one of the three given body types
+     */
+    private void chooseBody() {
+        Util.clearConsole();
+        System.out.println("Pick a body preset");
+        System.out.println("1. Fat\n2. Skinny\n3. Average");
+
+        int body = Util.enterInt(1, 3);
+        if (body == 1) {
+            this.currentBodyType = bodyType1;
+        } else if (body == 2) {
+            this.currentBodyType = bodyType2;
+        } else if (body == 3) {
+            this.currentBodyType = bodyType3;
+        }
+
+    }
+
+    /**
+     *contents inside this method will loop for the majority of the game
+     */
+    private void decision() { //block variable, you cannot use day outside of this for loop
+        Util.clearConsole();
+        System.out.println("How many days do you want to give yourself for your best you?");
+        int dayNumbers = Util.enterInt(1, 500);
+        Util.clearConsole();
+        System.out.println("You have " + dayNumbers + " days to fulfill your goals, " + currentBodyType.getName());
+        System.out.println(currentBodyType);
+        Util.pauseConsole();
+
+        String text = """
+        What do you want to do?
+        1. Go to the Gym
+        2. Eat food
+        3. Go for a run
+        4. Rest for the day
+        """;
+        System.out.println(text);
+        for (int day=dayNumbers;day>=1;day--) { //incrementing for loop
+            int option = Util.enterInt(1,4);
+            if (option == 1) {
+                Util.clearConsole();
+                System.out.println("What do you want to do at the gym?");
+                System.out.println("1. Bench Press\n2. Squat\n3. Run on the treadmill");
+                option = Util.enterInt(1,3);
+                if (option == 1) { //benchpress chosen
+                    benchPress();
+                } else if (option == 2) { //squat chosen
+                    squat();
+                } else if (option == 3) { //treadmill chosen
+                    Util.clearConsole();
+                    currentBodyType.setConsecutiveWorkoutDays(0); //running is rest as it does not heavily train muscles
+                    System.out.println("How many miles do you want to run?");
+                    System.out.println("Your current max run distance is: " + currentBodyType.getMaxRunDistance());
+                    int run = Util.enterInt(1,1000);
+                    if (run > currentBodyType.getMaxRunDistance() + 1) {
+                        System.out.println("You can't run that far! One day has been wasted");
+                    }
+                    else {
+                        currentBodyType.setWeight(currentBodyType.getWeight() - 4);
+                        currentBodyType.setLbmPercentage(currentBodyType.getLbmPercentage() + 0.5);
+                        currentBodyType.setFatPercentage(currentBodyType.getFatPercentage() - 0.5);
+                        System.out.println("LBM increased by 0.5\nFat decreased by 0.5\nWeight decreased by 4");
+                    } if (run > currentBodyType.getMaxRunDistance() - 1) { //increases max run distance after running
+                        currentBodyType.setMaxRunDistance(run);
+                        System.out.println("Your max distance has increased to " + currentBodyType.getMaxRunDistance() + "!");
+
+                    }
+                }
+            } else if (option == 2) { // eat food
+                Util.clearConsole();
+                System.out.println("What do you want to eat?");
+                System.out.println("1. Chicken\n2. Salad\n3. Burger");
+                int food = Util.enterInt(1,3);
+                if (food == 1 ) { //adds 5 weight and 2 lbm, subtracts 2 fat
+                    eatChicken();
+                }
+                else if (food == 2 ) { //subtracts 4 weight
+                    eatSalad();
+                }
+                else if (food == 3) { //adds 5 weight and 2 fat, subtracts 2 lbm
+                    eatBurger();
+                }
+            } if (option == 3) { // go for a run
+                Util.clearConsole();
+                currentBodyType.setConsecutiveWorkoutDays(0); //running is rest as it does not heavily train muscles
+                System.out.println("How many miles do you want to run? Note that you cannot run over a mile over your max");
+                System.out.println("Your current max run distance is: " + currentBodyType.getMaxRunDistance());
+                int run = Util.enterInt(1, 1000);
+                if (run > currentBodyType.getMaxRunDistance() + 1) {
+                    System.out.println("You can't run that far! One day has been wasted");
+                }
+                else {
+                    currentBodyType.setWeight(currentBodyType.getWeight() - 4);
+                    currentBodyType.setLbmPercentage(currentBodyType.getLbmPercentage() + 0.5);
+                    currentBodyType.setFatPercentage(currentBodyType.getFatPercentage() - 0.5);
+                    System.out.println("LBM increased by 0.5\nFat decreased by 0.5\nWeight decreased by 4");
+                } if (run > currentBodyType.getMaxRunDistance() - 1) { //increases max run distance after running
+                    currentBodyType.setMaxRunDistance(run);
+                    System.out.println("Your max distance has increased to " + currentBodyType.getMaxRunDistance() + "!");
+                } else if (option == 4) {
+                    currentBodyType.setConsecutiveWorkoutDays(0);
+                    currentBodyType.setLbmPercentage(currentBodyType.getLbmPercentage() + 0.5);
+                    currentBodyType.setFatPercentage(currentBodyType.getFatPercentage() - 0.5);
+                    System.out.println("You rested for the day");
+                    // You decide to rest for the day
+                }
+
+            }
+            Util.pauseConsole();
+            Util.clearConsole();
+            System.out.println("You have " + day + " days left to fulfill your goals, " + currentBodyType.getName());
+            System.out.println(currentBodyType);
+            Util.pauseConsole();
+
+            text = """
+          What do you want to do?
+          1. Go to the Gym
+          2. Eat food
+          3. Go for a run
+          4. Rest for the day
+          """;
+            System.out.println(text);
+        }
+    }
+
+
+    /**
+     *Precondition:
+     *decision() must be completely run before this method
+     *Postcondition:
+     *prints game over screen
+     */
+    private void endScreen() {
+        Util.clearConsole();
+        System.out.println("You have reached the deadline of your goal!");
+        System.out.println("Your final stats are:");
+        System.out.println(currentBodyType);
+
+        System.out.println("\nYour old stats are:");
+        if (currentBodyType == bodyType1) {
+            System.out.println("Name: " + currentBodyType.getName() + "\nWeight: 200\nBody Fat %: 30.0\nLean Body Mass %: 70.0\nMax Bench: 150\nMax Squat: 170\nMax Run Distance: 1");
+        } else if (currentBodyType == bodyType2) {
+            System.out.println("Name: " + currentBodyType.getName() + "\nWeight: 100\nBody Fat %: 10.0\nLean Body Mass %: 90.0\nMax Bench: 90\nMax Squat: 100\nMax Run Distance: 3");
+        } else if (currentBodyType == bodyType3) {
+            System.out.println("Name: " + currentBodyType.getName() + "\nWeight: 140\nBody Fat %: 15.0\nLean Body Mass %: 75.0\nMax Bench: 120\nMax Squat: 140\nMax Run Distance: 2");
+        }
+    }
+
+    /**
+     *run when the user wants to do a bench press
+     *<br>Precondition:
+     *(option == 1)
+     *<br>Postcondition:
+     *<br>LbmPercentage increased by 0.5
+     *<br>FatPercentage decreased by 0.5
+     *
+     *if the bench amount cannot be lifted then:
+     *lbmPercentage decreased by 0.5
+     *fatPercentage increased by 0.5
+     *
+     *if bench amount is under 5 over max:
+     *maxBench increased to amount the user benched
+     */
+    private void benchPress() {
+        Util.clearConsole();
+        System.out.println("How much lbs do you want to bench?");
+        System.out.println("Your current max is: " + currentBodyType.getMaxBench());
+        int bench = Util.enterInt(1,1000);
+        if (currentBodyType.getConsecutiveWorkoutDays() >= 5) {
+            System.out.println("You have not been taking enough rest! You wasted one day.");
+        }
+        else if (bench > currentBodyType.getMaxBench() + 5) {
+            System.out.println("You can't bench that much! You are injured in the process");
+            currentBodyType.setLbmPercentage(currentBodyType.getLbmPercentage() - 0.5);
+            currentBodyType.setFatPercentage(currentBodyType.getFatPercentage() + 0.5);
+            currentBodyType.setWeight(currentBodyType.getWeight() - 2);
+            System.out.println("LBM decreased by 0.5\nFat increased by 0.5\nWeight decreased by 2");
+        }
+        else {
+            currentBodyType.setLbmPercentage(currentBodyType.getLbmPercentage() + 0.5);
+            currentBodyType.setFatPercentage(currentBodyType.getFatPercentage() - 0.5);
+            System.out.println("LBM incresaed by 0.5\nFat decreased by 0.5");
+            if (bench > currentBodyType.getMaxBench() - 5) { //increases max bench after working out
+                currentBodyType.setMaxBench(bench);
+                System.out.println("Your max bench has increased to " + currentBodyType.getMaxBench() + "!");
+            }
+        }
+    }
+
+    /**
+     *run when the user wants to do a squat
+     *<br>Precondition:
+     *(option == 2)
+     *<br>Postcondition:
+     *<br>LbmPercentage increased by 0.5
+     *<br>FatPercentage decreased by 0.5
+     *
+     *if the squat amount cannot be lifted then:
+     *lbmPercentage decreased by 0.5
+     *fatPercentage increased by 0.5
+     *
+     *if squat amount is under 5 over max:
+     *maxSquat increased to amount the user squatted
+     */
+    private void squat() {
+        Util.clearConsole();
+        System.out.println("How much lbs do you want to squat?");
+        System.out.println("Your current max is: " + currentBodyType.getMaxSquat());
+        int squat = Util.enterInt(1,1000);
+        if (currentBodyType.getConsecutiveWorkoutDays() >= 4) {
+            System.out.println("You have not been taking enough rest! You wasted one day.");
+        }
+        else if (squat > currentBodyType.getMaxSquat() + 5) { // subtracts 2 lbm and 2 weight, adds 2 fat
+            System.out.println("You can't squat that much! You are injured in the process");
+        } else {
+            currentBodyType.setLbmPercentage(currentBodyType.getLbmPercentage() + 0.5);
+            currentBodyType.setFatPercentage(currentBodyType.getFatPercentage() - 0.5);
+            currentBodyType.setWeight(currentBodyType.getWeight() - 2);
+            System.out.println("LBM increased by 0.5\nFat decreased by 0.5");
+            if (squat > currentBodyType.getMaxSquat() - 5) { //increases max squat after working out
+                currentBodyType.setMaxSquat(squat);
+                System.out.println("Your max squat has increased to " + currentBodyType.getMaxSquat() + "!");
+            }
+        }
+    }
+
+    /**
+     *run when the user wants to eat chicken
+     *<br>Precondition:
+     *(food == 1)
+     *<br>Postcondition:
+     *<br>LbmPercentage increased by 0.5
+     *<br>FatPercentage decreased by 0.5
+     */
+    private void eatChicken() {
+        Util.clearConsole();
+        currentBodyType.setWeight(currentBodyType.getWeight() + 5);
+        currentBodyType.setFatPercentage(currentBodyType.getFatPercentage() - 0.5);
+        currentBodyType.setLbmPercentage(currentBodyType.getLbmPercentage() + 0.5);
+        System.out.println("LBM increased by 0.5\nFat decreased by 0.5\nWeight increased by 5");
+    }
+
+    /**
+     *run when the user wants to eat salad
+     *<br>Precondition:
+     *(food == 2)
+     *<br>Postcondition:
+     *<br>Weight decreased by 3
+     */
+    private void eatSalad() {
+        Util.clearConsole();
+        currentBodyType.setWeight(currentBodyType.getWeight() - 3);
+        System.out.println("Weight decreased by 3");
+    }
+
+    /**
+     *run when the user wants to eat burger
+     *<br>Precondition:
+     *(food == 3)
+     *<br>Postcondition:
+     *<br>LbmPercentage decreased by 0.7
+     *<br>FatPercentage increased by 0.7
+     *<br>Weight increased by 5
+     */
+    private void eatBurger() {
+        Util.clearConsole();
+        currentBodyType.setWeight(currentBodyType.getWeight() + 10);
+        currentBodyType.setFatPercentage(currentBodyType.getFatPercentage() + 0.7);
+        currentBodyType.setLbmPercentage(currentBodyType.getLbmPercentage() - 0.7);
+        System.out.println("LBM decreased by 0.7\nFat increased by 0.7\nWeight increased by 5");
+    }
+}
+
+
+
+
+
+
+
+
+
+
---
+++ app/src/main/java/com/poly/polyapcsafinalproject23_24/SengMan.java
@@ -0,0 +1,279 @@
+package com.poly.polyapcsafinalproject23_24;
+
+import com.poly.polyapcsafinalproject23_24.Contestant;
+
+public class SengMan extends Contestant {
+
+
+    /**
+     * player's name
+     */
+    private String name;
+    /**
+     * player's weight
+     */
+    private double weight;
+    /**
+     * player's body fat percentage
+     */
+    private double fatPercentage;
+    /**
+     * player's lean body mass percentage
+     */
+    private double lbmPercentage;
+    /**
+     * player's max benchpress weight
+     */
+    private int maxBench;
+    /**
+     * player's max squat weight
+     */
+    private int maxSquat;
+    /**
+     * player's max run distance in miles
+     */
+    private int maxRunDistance;
+    /**
+     * tracks how much days of strength training in a row
+     */
+    private int consecutiveWorkoutDays;
+    /**
+     * ID of the player, assigned in constructor
+     */
+    private final int ID;
+    /**
+     * tracks number of men created
+     */
+    private static int numMen;
+
+    /**
+     *default constructor
+     *<br>Postcondition: man with name as empty string, all variables as 0
+     */
+    public Man() {
+        name = "";
+        weight = 0;
+        fatPercentage = 0;
+        lbmPercentage = 0;
+        maxBench = 0;
+        maxSquat = 0;
+        maxRunDistance = 0;
+        consecutiveWorkoutDays = 0;
+        numMen++;
+        this.ID = numMen;
+    }
+
+    /**
+     * Constructor that initializes all attributes
+     * <br>Preconditions: name must be nonull, all other values must be non-negative
+     * <br>Postconditions: contestant with given name, weight, fat percentage, lean body mass percentage, max bench, max squat, max run distance, consecutive, ID assigned
+     * @param name                   name of the player
+     * @param weight                 weight of the player ( >=0 )
+     * @param fatPercentage          fat percentage of the player ( >=0 )
+     * @param lbmPercentage          lean body mass percentage of the player ( >=0 )
+     * @param maxBench               max bench press weight of the player ( >=0 )
+     * @param maxSquat               max squat weight of the player ( >=0 )
+     * @param maxRunDistance         max run distance of the player ( >=0 )
+     * @param consecutiveWorkoutDays consecutive workout days of the player ( >=0 )
+     */
+    public Man(String n, double w, double f, double c, int mb, int ms, int mr, int cw) {
+        name = n;
+        weight = w;
+        fatPercentage = f;
+        lbmPercentage = c;
+        maxBench = mb;
+        maxSquat = ms;
+        maxRunDistance = mr;
+        consecutiveWorkoutDays = cw;
+        numMen++;
+        this.ID = numMen;
+    }
+    /**
+     * Constructor that initializes name
+     *<br>Preconditions: name must be nonull
+     *<br>Postconditions: contestent with given name, all other values as 0
+     */
+    public Man(String name) {
+        this.name = "";
+        weight = 0;
+        fatPercentage = 0;
+        lbmPercentage = 0;
+        maxBench = 0;
+        maxSquat = 0;
+        maxRunDistance = 0;
+        consecutiveWorkoutDays = 0;
+        numMen++;
+        this.ID = numMen;
+    }
+
+
+
+
+
+
+    // getters
+    /**
+     * gets the name of the player
+     * @return name of the player
+     */
+    public String getName() {
+        return name;
+    }
+    /**
+     * gets the weight of the player
+     * @return weight of the player
+     */
+
+    public double getWeight() {
+        return weight;
+    }
+    /**
+     * gets the body fat percentage of the player
+     * @return body fat percentage of the player
+     */
+    public double getFatPercentage() {
+        return fatPercentage;
+    }
+    /**
+     * gets the lean body mass percentage of the player
+     * @return lean body mass percentage of the player
+     */
+    public double getLbmPercentage() {
+        return lbmPercentage;
+    }
+    /**
+     * gets the max bench of the plauer
+     * @return max bench of the player
+     */
+    public int getMaxBench() {
+        return maxBench;
+    }
+    /**
+     * gets the max squat of the player
+     * @return max squat of the player
+     */
+    public int getMaxSquat() {
+        return maxSquat;
+    }
+    /**
+     * gets the max run distance of the player
+     * @return max run distance of the player
+     */
+    public int getMaxRunDistance() {
+        return maxRunDistance;
+    }
+    /**
+     * gets the consecutive workout days of the player
+     * @return consecutive workout days of the player
+     */
+    public int getConsecutiveWorkoutDays() {
+        return consecutiveWorkoutDays;
+    }
+
+    //setters
+
+    /**
+     *sets the number of the player's weight
+     *<br>Precondition: weight must be non-negative
+     *<br>Postcondition: weight is set
+     *@param     weight     weight of the player
+     */
+    public void setWeight(double w) {
+        this.weight = w;
+        if (weight < 0) {
+            weight = 0;
+        }
+    }
+    /**
+     *sets the number of the player's body fat percentage
+     *<br>Precondition: fatPercentage must be non-negative
+     *<br>Postcondition: fatPercentage is set
+     *@param     fatPercentage     fat percentage of the player
+     */
+    public void setFatPercentage(double f) {
+
+        this.fatPercentage = f;
+        if (fatPercentage < 0) {
+            fatPercentage = 0;
+        }
+
+    }
+    /**
+     *sets the number of the player's lean body mass percentage
+     *<br>Precondition: lbmPercentage must be non-negative
+     *<br>Postcondition: lbmPercentage is set
+     *@param     lbmPercentage     lean body mass percentage of the player
+     */
+    public void setLbmPercentage(double m) {
+
+        this.lbmPercentage = m;
+        if (lbmPercentage < 0) {
+            lbmPercentage = 0;
+        }
+    }
+    /**
+     *sets the number of the player's max benchpress
+     *<br>Precondition: maxBench must be non-negative
+     *<br>Postcondition: maxBench is set
+     *@param     maxBench     max bench press weight of the player
+     */
+    public void setMaxBench(int mb) {
+        this.maxBench = mb;
+
+    }
+    /**
+     *sets the number of the player's max squat
+     *<br>Precondition: maxSquat must be non-negative
+     *<br>Postcondition: maxSquat is set
+     *@param     maxSquat     max squat weight of the player
+     */
+    public void setMaxSquat(int ms) {
+        this.maxSquat = ms;
+
+    }
+    /**
+     *sets the number of the player's max run distance
+     *<br>Precondition: maxRunDistance must be non-negative
+     *<br>Postcondition: maxRunDistance is set
+     *@param     maxRunDistance     max run distance of the player
+     */
+    public void setMaxRunDistance(int mr) {
+        this.maxRunDistance = mr;
+
+    }
+    /**
+     *sets the number of the player's consecutive workout days
+     *<br>Precondition: consecutiveWorkoutDays must be non-negative
+     *<br>Postcondition: consecutiveWorkoutDays is set
+     *@param     consecutiveWorkoutDays     consecutive workout days of the player
+     */
+    public void setConsecutiveWorkoutDays(int cw) {
+        this.consecutiveWorkoutDays = cw;
+    }
+    /**
+     *toString method
+     *@return player's stats
+     */
+    //toString
+    public String toString() {
+        return "Name: " + name + "\nWeight: " + weight + "\nBody Fat %: " + fatPercentage + "\nLean Body Mass %: " + lbmPercentage + "\nMax Benchpress: " + maxBench + "\nMax Squat: " + maxSquat + "\nMax Run Distance: " + maxRunDistance;
+
+
+    }
+    /**
+     *returns true if all attributes match between two men
+     * <br>Precondition: other must be non-null
+     *@param     other     the other man
+     *@return    true if all attributes match, false otherwise
+     */
+    public boolean equals(Man otherMan) {
+        if (this.name.equals(otherMan.name) && this.weight == otherMan.weight && this.fatPercentage == otherMan.fatPercentage && this.lbmPercentage == otherMan.lbmPercentage && this.maxBench == otherMan.maxBench && this.maxSquat == otherMan.maxSquat && this.maxRunDistance == otherMan.maxRunDistance && this.ID == otherMan.ID) {
+            return true;
+        }
+        return false;
+    }
+}
+
+
+
+
