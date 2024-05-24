00f8c062b3676f58aad665c639e0f5a7fb10d799
MMcGuinness07 <mcguinnessmichael78@gmail.com>
2024-05-24 11:51:55 -0700

    Buttons Do Things

--- app/src/main/java/com/poly/polyapcsafinalproject23_24/GameMcGuinnessMichael.java
+++ app/src/main/java/com/poly/polyapcsafinalproject23_24/GameMcGuinnessMichael.java
@@ -1,5 +1,6 @@
 package com.poly.polyapcsafinalproject23_24;
 
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -42,6 +43,10 @@ public class GameMcGuinnessMichael extends GameActivity {
 
         //display project title and description
          //util wuz hear
+        tvTitle.setText("Hit On Hot Person");
+        tvSubtitle.setText("");
+        tvStoryText.setText("You see an hot person. You think they are really good looking and you want to get their number. What do you do?")
+
         System.out.println("Hit on Hot person");
         System.out.println("You see an hot person. You think they are really good looking and you want to get their number. What do you do?");
 
@@ -67,22 +72,28 @@ public class GameMcGuinnessMichael extends GameActivity {
     3. Smolder
      """);
 
-        //util wuz hear
-        if (option == 1)
-        { //pickup line
-            points += 100;
-            lines();
-        }
-        else if (option == 2)
-        { //comedy
-            points += 200;
-            comedy();
-        }
-        else if (option == 3)
-        { //smolder line 65
-            points += 300;
-            smolder();
-        }
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                points += 100;
+                lines();
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                points += 200;
+                comedy();
+            }
+        });
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                points += 300;
+                smolder();
+            }
+        });
 
     }
 
@@ -94,39 +105,47 @@ public class GameMcGuinnessMichael extends GameActivity {
     1. Smolder into the distance
     2. Smolder at them
     """);
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (Math.random() > 0.5)
+                {
+                    System.out.println("They think you are staring at them. They are not interested in a stalker");
+                    points -= 15;
+                    defeat();
+                }
+                else
+                {
+                    System.out.println("They are into the smoldering. They give you their number and walk away");
+                    points += 100;
+                    success();
+                }
+            }
+        });
+
+
          //util wuz hear
-        if (option == 2)
-        { //smolder at them
-            if (Math.random() > 0.5)
-            {
-                System.out.println("They think you are staring at them. They are not interested in a stalker");
-                points -= 15;
-                defeat();
-            }
-            else
-            {
-                System.out.println("They are into the smoldering. They give you their number and walk away");
-                points += 100;
-                success();
-            }
-        }
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (Math.random() > 0.5)
+                {
+                    System.out.println("They don't notice you. Fail.");
+                    points -= 10;
+                    defeat();
+                }
+                else
+                {
+                    points += 50;
+                    noticeYou();
 
-        else if (option == 1)
-        { //smolder into distance
-            if (Math.random() > 0.5)
-            {
-                System.out.println("They don't notice you. Fail.");
-                points -= 10;
-                defeat();
+                    //line 106
+                }
             }
-            else
-            {
-                points += 50;
-                noticeYou();
+        });
 
-                //line 106
-            }
-        }
+
     }
 
     private void noticeYou() {
@@ -135,20 +154,26 @@ public class GameMcGuinnessMichael extends GameActivity {
           1. Break the stance and ask for their number
           2. Say nothing. Smolder even harder
           """);
-        //util wuz hear
-        if (option == 1)
-        {
-            System.out.println("You break the look and try to ask for their number but your voice cracks and you sound like an elderly goose. They are disgusted and lose all interest in you");
-            points -= 25;
-            defeat();
-        }
-        else if (option == 2)
-        {
-            System.out.println("You don't react to them approaching you. They are about to talk to you but get intimidated by the intensity of the look on your face. They are about to leave but suddenly you smolder harder. They fall apart and cant belive how powerful you look. They write down their number and stick it in your pocket and whisper 'call me' in your ear before walking away. ");
-            points += 575;
-            success();
 
-        }
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                System.out.println("You break the look and try to ask for their number but your voice cracks and you sound like an elderly goose. They are disgusted and lose all interest in you");
+                points -= 25;
+                defeat();
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                System.out.println("You don't react to them approaching you. They are about to talk to you but get intimidated by the intensity of the look on your face. They are about to leave but suddenly you smolder harder. They fall apart and cant belive how powerful you look. They write down their number and stick it in your pocket and whisper 'call me' in your ear before walking away. ");
+                points += 575;
+                success();
+            }
+        });
+
     }
 
     //comedy path
@@ -159,41 +184,55 @@ public class GameMcGuinnessMichael extends GameActivity {
       1. I just got rid of my vacuum cleaner. It sucked.
       2. Cosmetic surgery use to be such a touchy subject. Now you talk about botox and nobody raises an eyebrow.
       """);
-         //util wuz hear
-        if (option == 1) { //vacuum
-            points += 25;
-            weird();
-        }
-        else if (option == 2) { //botox
-            points += 50;
-            if (Math.random() > 0.5) {
-                System.out.println("They think you are insulting their appearance. They yell at you for being misogynistic and superficial and walk away");
-                points -= 15;
-                defeat();
 
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                points += 25;
+                weird();
             }
-            else {
-                botox();
-                //line 160- ***
-            }
+        });
 
-        }
-    }
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                points += 50;
+                if (Math.random() > 0.5) {
+                    System.out.println("They think you are insulting their appearance. They yell at you for being misogynistic and superficial and walk away");
+                    points -= 15;
+                    defeat();
+
+                }
+                else {
+                    botox();
+            }
+        });
+
+
+
 
     private void botox() {
         System.out.println("They ask you if you think that they actually look like they have gotten work done. What do you respond? \n1. Be nice and tell them they are beautiful \n2. Tell them they look like Caitlyn Jenner");
         //util wuz hear
-        if (option == 1){
-            System.out.println("They think you're sweet and they give you their number");
-            points += 115;
-            success();
-        }
 
-        else {
-            System.out.println("They start yelling at you and start calling you terrible names and storm away in disgust. You probably meant Kylie or Kendall but now you blundered it");
-            points -= 30;
-            defeat();
-        }
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("They think you're sweet and they give you their number");
+                points += 115;
+                success();
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("They start yelling at you and start calling you terrible names and storm away in disgust. You probably meant Kylie or Kendall but now you blundered it");
+                points -= 30;
+                defeat();
+            }
+        });
+
 
     }
 
@@ -204,19 +243,24 @@ public class GameMcGuinnessMichael extends GameActivity {
           1. How does a hot dog show apreciation? It franks you
           2. When does Thor like pop rap? When it's hammer time
           """);
-        //util wuz hear
-        if (option == 1){
-            System.out.println(" They feel bad for you and give you their number out of pity");
-            points += 3;
-            success();
 
-        }
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println(" They feel bad for you and give you their number out of pity");
+                points += 3;
+                success();
+            }
+        });
 
-        else {
-            System.out.println("They are visibly not interested. they tell you they have a boyfriend");
-            points += 8;
-            defeat();
-        }
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("They are visibly not interested. they tell you they have a boyfriend");
+                points += 8;
+                defeat();
+            }
+        });
 
 
     }
@@ -229,14 +273,22 @@ public class GameMcGuinnessMichael extends GameActivity {
     1. Do you live in a corn field? Beacuse I'm stalking you
     2. I just pooped in my bed, can I sleep in yours?
     """);
-          //util wuz hear
-        if (option == 1) {
-            color();
-        }
 
-        else {
-            weird();
-        }
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                defeat();
+            }
+        });
+
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                weird();
+            }
+        });
+
     }
 
     private void color() {
@@ -246,44 +298,65 @@ public class GameMcGuinnessMichael extends GameActivity {
         2. Brown
         3. Ignore the question and try another pickup line
           """);
-        //util wuz hear
-        if (option == 1) {
-            System.out.println("OMG you guessed correct! They call the cops on you and you go to prison for a long time. YOU LOSE! You don't get another chance. GAME OVER!");
-            points -= 350;
-            numLives -=5;
-            System.out.println("You got " + points + " points");
-        }
-        else if (option == 2){
-            confuse();
-        }
-        else {
-            System.out.println("You ask them 'Are you a parking ticket? Because I want to jam you under a car windshield' ");
-            if (Math.random() > 0.75) {
-                System.out.println(" Fail. What did you think would happen with a line like that?");
-                points -= 8;
-                defeat();
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("OMG you guessed correct! They call the cops on you and you go to prison for a long time. YOU LOSE! You don't get another chance. GAME OVER!");
+                points -= 350;
+                numLives -=5;
+                System.out.println("You got " + points + " points");
             }
-            else {
-                System.out.println(" They like that? They give you their number? WHAT? What kind of idiot who has never talked to someone wrote this stupid quiz?");
-                points += 40;
-                success();
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                confuse();
             }
-        }
+        });
+
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("You ask them 'Are you a parking ticket? Because I want to jam you under a car windshield' ");
+                if (Math.random() > 0.75) {
+                    System.out.println(" Fail. What did you think would happen with a line like that?");
+                    points -= 8;
+                    defeat();
+                }
+                else {
+                    System.out.println(" They like that? They give you their number? WHAT? What kind of idiot who has never talked to someone wrote this stupid quiz?");
+                    points += 40;
+                    success();
+                }
+            }
+        });
+
+
     }
 
     private void confuse() {
         System.out.println("You are wrong and they are really confused. Last chance. What do you do? \n1. Cry\n2. Tell them you love them");
         //util wuz hear
-        if (option == 1) {
-            System.out.println("You start crying. They take pity on you and give you their number.");
-            points += 7;
-            success();
-        }
-        else {
-            System.out.println("You tell them you love them. They get visibly disgusted. They tell you they have a boyfriend and walk away. Now you start to cry for real.");
-            points -= 13;
-            defeat();
-        }
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("You start crying. They take pity on you and give you their number.");
+                points += 7;
+                success();
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("You tell them you love them. They get visibly disgusted. They tell you they have a boyfriend and walk away. Now you start to cry for real.");
+                points -= 13;
+                defeat();
+            }
+        });
+
     }
 
 
