531458449f445af26238d7e7461cb72088f86ca6
ranseschavez <chavezranses444@gmail.com>
2024-06-06 12:00:41 -0700

    day 5 complete

--- app/src/main/java/com/poly/polyapcsafinalproject23_24/GameChavezRanses2.java
+++ app/src/main/java/com/poly/polyapcsafinalproject23_24/GameChavezRanses2.java
@@ -34,7 +34,7 @@ public class GameChavezRanses2 extends GameActivity {
         btn3 = findViewById(R.id.btn_3);
 
         tvTitle.setText("THE MATRIX");
-        tvSubTitle.setText("High School Edition");
+        tvSubTitle.setText("Those scared never live .");
 
         //initialize number of lives
         numLives = 5;
@@ -55,6 +55,10 @@ public class GameChavezRanses2 extends GameActivity {
 
     private void start()
     {
+
+        ivStory.setImageResource(R.drawable.im_chavezranses_blackguy);
+
+
         tvStoryText.setText("Escaping the Matrix, how blind are we?");
         //Util.enterInt was here
 
@@ -90,6 +94,9 @@ public class GameChavezRanses2 extends GameActivity {
 
     private void giveUp()
     {
+
+        ivStory.setImageResource(R.drawable.im_chavezranses_emojishrug);
+
         //Util.clearConsole was here
         tvStoryText.setText("What would you like to do on your free time?");
         //Util.enterInt was here
@@ -117,6 +124,9 @@ public class GameChavezRanses2 extends GameActivity {
     //____STAYING HOME CHOICE____
     private void stayHome()
     {
+
+        ivStory.setImageResource(R.drawable.im_chavezranses_cuteteddy);
+
         //Util.clearConsole was here
         tvStoryText.setText("You decide to stay home and be FAT");
         //Util.enterInt was here
@@ -144,6 +154,9 @@ public class GameChavezRanses2 extends GameActivity {
     }
     private void goMimis()  //DEFEAT
     {
+
+        ivStory.setImageResource(R.drawable.im_chavezranses_fatguy);
+
         tvStoryText.setText("After a long beautiful uneventful night, you wake up and see the heavenly stairs of heaven since you died in your sleep for being FAT");
 
         setAllBtnsVisible();
@@ -162,6 +175,8 @@ public class GameChavezRanses2 extends GameActivity {
 
     private void watchVideos()
     {
+        ivStory.setImageResource(R.drawable.im_chavezranses_conspiracy);
+
         //Util.clearConsole was here
         tvStoryText.setText("You decide to enjoy the rest of your night by watching conspiracy videos until you notice a man watching you through your window, what shall you do?");
         //Util.clearConsole was here
@@ -188,6 +203,9 @@ public class GameChavezRanses2 extends GameActivity {
     }
     private void runToParents() //DEFEAT
     {
+
+        ivStory.setImageResource(R.drawable.im_chavezranses_beginning);
+
         //Util.clearConsole was here
         tvStoryText.setText("In your attempts to get your parents the man watching you appears behind you the moment you turned your back to him, you die, the matrix show their true power");
         //Util.clearConsole was here
@@ -207,6 +225,8 @@ public class GameChavezRanses2 extends GameActivity {
     }
     private void hideBed()  //SAD VICTORY
     {
+        ivStory.setImageResource(R.drawable.im_chavezranses_thematrix);
+
         //Util.clearConsole was here
         tvStoryText.setText("You hide and close your eyes. Once you open them everything is white. You try recalling what had just happened yet cannot remember a single thing. Funny . Do I know you?");
         //Util.clearConsole was here
@@ -216,6 +236,8 @@ public class GameChavezRanses2 extends GameActivity {
     //_________GOING OUT CHOICE______
     private void goOut()
     {
+        ivStory.setImageResource(R.drawable.im_chavezranses_manstarring);
+
         //Util.clearConsole was here
         tvStoryText.setText("You go out and have the time of your life until you notice a man starring at you");
         //Util.clearConsole was here
@@ -241,6 +263,8 @@ public class GameChavezRanses2 extends GameActivity {
     }
     private void pressIssue()
     {
+        ivStory.setImageResource(R.drawable.im_chavezranses_confrontman);
+
         //Util.clearConsole was here
         tvStoryText.setText("You run up on the man real gangsta style");
         //Util.clearConsole was here
@@ -267,6 +291,8 @@ public class GameChavezRanses2 extends GameActivity {
 
     private void leftHook() //DEFEAT
     {
+        ivStory.setImageResource(R.drawable.im_chavezranses_knockedout);
+
         //Util.clearConsole was here
         tvStoryText.setText("Once you threw your punch, the supposed man was never a man, you turn around and BOOM.");
 
@@ -287,6 +313,8 @@ public class GameChavezRanses2 extends GameActivity {
 
     private void pushHim()  //SAD VICTORY
     {
+        ivStory.setImageResource(R.drawable.im_chavezranses_inblackhole);
+
         //Util.clearConsole was here
         tvStoryText.setText("You fall inside the man's cloak and continue falling, and falling, and falling");
 
@@ -298,6 +326,8 @@ public class GameChavezRanses2 extends GameActivity {
 
     private void runAway()
     {
+        ivStory.setImageResource(R.drawable.im_chavezranses_cryingboy);
+
         //Util.clearConsole was here
         tvStoryText.setText("You run away and luckily survive.");
 
@@ -326,6 +356,9 @@ public class GameChavezRanses2 extends GameActivity {
 
 
     private void goPublic() {
+
+        ivStory.setImageResource(R.drawable.im_chavezranses_crowd);
+
         //Util.clearConsole was here
         tvStoryText.setText("You choose to go public, how should you approach this?");
         //Util.clearConsole was here
@@ -352,6 +385,8 @@ public class GameChavezRanses2 extends GameActivity {
 
     private void youtube()  //____YOUTUBE CHOICE_____
     {
+        ivStory.setImageResource(R.drawable.im_chavezranses_deal);
+
         //Util.clearConsole was here
         tvStoryText.setText("You gain a mass following and are given the choice to make a deal or continue on your own");
         //Util.clearConsole was here
@@ -377,7 +412,7 @@ public class GameChavezRanses2 extends GameActivity {
     }
     private void deal() //DEFEAT
     {
-        //Util.clearConsole was here
+        ivStory.setImageResource(R.drawable.im_chavezranses_doordash);
 
         tvStoryText.setText("Without reading any contracts you make this deal and realize, you made a deal with doordash...");
         tvStoryText.setText("You are forever known as doordash man");
@@ -397,6 +432,9 @@ public class GameChavezRanses2 extends GameActivity {
     }
     private void loner()
     {
+
+        ivStory.setImageResource(R.drawable.im_chavezranses_matrixboy);
+
         //Util.clearConsole was here
         tvStoryText.setText("Working alone increased your following and you are now known as the MATRIX MAN. How should you continue your legacy?");
         //Util.clearConsole was here
@@ -422,7 +460,8 @@ public class GameChavezRanses2 extends GameActivity {
     }
     private void bob() //DEFEAT
     {
-        //Util.clearConsole was here
+
+        ivStory.setImageResource(R.drawable.im_chavezranses_bob);
 
         tvStoryText.setText("Bob");
 
@@ -448,6 +487,9 @@ public class GameChavezRanses2 extends GameActivity {
 
     private void matrixMan()  //VICTORY
     {
+
+        ivStory.setImageResource(R.drawable.im_chavezranses_kingmatrix);
+
         //Util.clearConsole was here
         tvStoryText.setText("THE WHOLE WORLD KNOWS YOUR NAME AND OBEY YOU");
 
@@ -459,6 +501,8 @@ public class GameChavezRanses2 extends GameActivity {
 
     private void preachingWeirdo() //_____PREACHING CHOICE____
     {
+        ivStory.setImageResource(R.drawable.im_chavezranses_mallkid);
+
         //Util.clearConsole was here
         tvStoryText.setText("You go to the mall daily and preach about the Matrix. You can either stick with malls or preach everywhere, including hospitals");
         //Util.clearConsole was here
@@ -485,6 +529,8 @@ public class GameChavezRanses2 extends GameActivity {
 
     private void malls() //DEFEAT
     {
+        ivStory.setImageResource(R.drawable.im_chavezranses_loser);
+
         //Util.clearConsole was here
 
         tvStoryText.setText("After some time your crush sees you preaching at the mall and blocks you on all social media");
@@ -507,6 +553,8 @@ public class GameChavezRanses2 extends GameActivity {
 
     private void youAreWeird()  //SAD VICTORY
     {
+        ivStory.setImageResource(R.drawable.im_chavezranses_matrixworld);
+
         //Util.clearConsole was here
         tvStoryText.setText("Surprisingly people begin supporting you and start questioning their way of living");
 
@@ -525,6 +573,8 @@ public class GameChavezRanses2 extends GameActivity {
 
     private void ventureOut()
     {
+        ivStory.setImageResource(R.drawable.im_chavezranses_emojiquestion);
+
         //Util.clearConsole was here
         tvStoryText.setText("You have decided to venture out, where shall you go?");
         //Util.clearConsole was here
@@ -551,6 +601,8 @@ public class GameChavezRanses2 extends GameActivity {
 
     private void stayLocal()  //_____STAYING LOCAL CHOICE___________
     {
+        ivStory.setImageResource(R.drawable.im_chavezranses_area51);
+
         //Util.clearConsole was here
         tvStoryText.setText("Since you have chosen to stay local you arrive at Area 51, there are two ways to getting inside");
         //Util.clearConsole was here
@@ -576,6 +628,8 @@ public class GameChavezRanses2 extends GameActivity {
     }
     private void underground()  //SAD VICTORY
     {
+        ivStory.setImageResource(R.drawable.im_chavezranses_boomba);
+
         //Util.clearConsole was here
         tvStoryText.setText("You take the underground path and fall into an underground civilization called the Bombaclats");
 
@@ -588,6 +642,8 @@ public class GameChavezRanses2 extends GameActivity {
 
     private void jumpFence()
     {
+        ivStory.setImageResource(R.drawable.im_chavezranses_darkarea51);
+
         //Util.clearConsole was here
         tvStoryText.setText("You're in! It's so dark...where now?");
         //Util.clearConsole was here
@@ -614,6 +670,9 @@ public class GameChavezRanses2 extends GameActivity {
 
     private void forward()
     {
+
+        ivStory.setImageResource(R.drawable.im_chavezranses_containedblackhole);
+
         //Util.clearConsole was here
         tvStoryText.setText("You notice a door and enter, inside the room you get hit with the presence of a contained black hole, do you touch it?");
         //Util.clearConsole was here
@@ -639,6 +698,8 @@ public class GameChavezRanses2 extends GameActivity {
     }
     private void blackHole()  //VICTORY
     {
+        ivStory.setImageResource(R.drawable.im_chavezranses_multiverse);
+
         //Util.clearConsole was here
         tvStoryText.setText("You get sucked in and observe knowledge no man can bare to witness");
 
@@ -649,6 +710,8 @@ public class GameChavezRanses2 extends GameActivity {
     }
     private void noTouch() //DEFEAT
     {
+        ivStory.setImageResource(R.drawable.im_chavezranses_testdummy);
+
         //Util.clearConsole was here
 
         tvStoryText.setText("Because of your fear, you try turning back yet get trapped inside and get caught");
@@ -671,6 +734,8 @@ public class GameChavezRanses2 extends GameActivity {
 
     private void wallHugger() //DEFEAT
     {
+        ivStory.setImageResource(R.drawable.im_chavezranses_wallhugger);
+
         //Util.clearConsole was here
 
         tvStoryText.setText("You hug the wall and eventually discover a manlike figure");
@@ -693,6 +758,8 @@ public class GameChavezRanses2 extends GameActivity {
 
     private void acrossSeas()  //_____ACROSS SEAS CHOICE___________
     {
+        ivStory.setImageResource(R.drawable.im_chavezranses_acrossseas);
+
         //Util.clearConsole was here
         tvStoryText.setText("ACROSS SEAS WE GO! Now that your across seas you go to the one man who knows of the Matrix, Andrew Tate to ask questions.");
         //Util.clearConsole was here
@@ -719,6 +786,8 @@ public class GameChavezRanses2 extends GameActivity {
 
     private void direct()  //VICTORY
     {
+        ivStory.setImageResource(R.drawable.im_chavezranses_matrixtate);
+
         //Util.clearConsole was here
         tvStoryText.setText("He respects your directness and tells you everything he knows");
 
@@ -730,6 +799,8 @@ public class GameChavezRanses2 extends GameActivity {
 
     private void indirect() //SAD DEFEAT
     {
+        ivStory.setImageResource(R.drawable.im_chavezranses_ponder);
+
         //Util.clearConsole was here
         tvStoryText.setText("Since you were indirect you can either choose a money distraction or physical distraction.");
         //Util.clearConsole was here
@@ -756,6 +827,8 @@ public class GameChavezRanses2 extends GameActivity {
 
     private void money() //DEFEAT
     {
+        ivStory.setImageResource(R.drawable.im_chavezranses_strongtate);
+
         //Util.clearConsole was here
 
         tvStoryText.setText("Well... you tried flexing your money");
@@ -777,6 +850,8 @@ public class GameChavezRanses2 extends GameActivity {
     }
     private void pushups() //DEFEAT
     {
+        ivStory.setImageResource(R.drawable.im_chavezranses_fatloser);
+
         //Util.clearConsole was here
 
         tvStoryText.setText("You try doing 5 push-ups and eat the pavement after your third");
