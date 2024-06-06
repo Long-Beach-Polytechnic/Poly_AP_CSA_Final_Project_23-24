742735690c7844fc3536ff329c423a87a8aa7b82
Benjamin A. Valentine <benjiv2007@gmail.com>
2024-06-06 11:55:39 -0700

    Be very very efficient; a lot of progress had been made. You just need to "transfer" (?) but it'll be quick. Come in on 8th.

--- app/src/main/java/com/poly/polyapcsafinalproject23_24/GameValentineBenjaminA.java
+++ app/src/main/java/com/poly/polyapcsafinalproject23_24/GameValentineBenjaminA.java
@@ -1,7 +1,11 @@
 package com.poly.polyapcsafinalproject23_24;
 
 
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class GameValentineBenjaminA extends GameActivity {
 
@@ -11,9 +15,14 @@ public class GameValentineBenjaminA extends GameActivity {
 
     EditText etView;
 
+    ImageView ivPlayer, ivMain;
+    TextView tvText, tvMoneyVal, tvDriverId;
 
+    TextView[] tvHistory;
 
+    Button btn1, btn2, btn3;
 
+    ProgressBar pbWantedLevel, pbMoralLevel;
 
 
 
@@ -22,10 +31,37 @@ public class GameValentineBenjaminA extends GameActivity {
     //  use loops as nessesary
     public void run()
     {
+        setContentView(R.layout.activity_valentineben_main);
+        etView = findViewById(R.id.et_name);
+        ivPlayer = findViewById(R.id.iv_player);
+        ivMain = findViewById(R.id.iv_main);
+        tvText = findViewById(R.id.tv_text);
+        tvMoneyVal = findViewById(R.id.tv_money);
+        tvDriverId = findViewById(R.id.tv_driver_id);
+
+        tvHistory = new TextView[8];
+        tvHistory[0] = findViewById(R.id.tv_history_1);
+        tvHistory[1] = findViewById(R.id.tv_history_2);
+        tvHistory[2] = findViewById(R.id.tv_history_3);
+        tvHistory[3] = findViewById(R.id.tv_history_4);
+        tvHistory[4] = findViewById(R.id.tv_history_5);
+        tvHistory[5] = findViewById(R.id.tv_history_6);
+        tvHistory[6] = findViewById(R.id.tv_history_7);
+        tvHistory[7] = findViewById(R.id.tv_history_8);
+
+        btn1 = findViewById(R.id.btn_1);
+        btn2 = findViewById(R.id.btn_2);
+        btn3 = findViewById(R.id.btn_3);
+
+        pbWantedLevel = (ProgressBar) findViewById(R.id.pb_wanted_level);
+        pbMoralLevel = (ProgressBar) findViewById(R.id.pb_moral_level);
+
+
+
+
         System.out.println("You wake up in a strange place. You don't know where or who you are, but you walk out of the alley way you woke up in and see that the city is bright. You find yourself a cheap house with the insignificant amount of money that you had woken up with, and you ultimately decide to live a life of crime. After all, you don't have anything to live for... right?");
         System.out.println("What is your name?");
-        EditText name;
-        ValentineCriminal player1 = new ValentineCriminal(name);
+        ValentineCriminal player1 = new ValentineCriminal(etView.getText().toString());
         System.out.println("How many people will be playing with you?");
     }
 
