810a0e7097e712d7d65b12ef71e1708a9e562f2c
Aaron <201112775@lbschools.net>
2024-06-05 17:35:20 -0700

    Get images all of battle logic is replaced with static messages that continue forever

--- app/src/main/java/com/poly/polyapcsafinalproject23_24/GameSannAaron.java
+++ app/src/main/java/com/poly/polyapcsafinalproject23_24/GameSannAaron.java
@@ -171,46 +171,22 @@ public class GameSannAaron extends GameActivity {
          */
         public void viridianForest()
         {
-            String text10 = "Brock, the Gym Leader of Pewter City, has really strong and high-leveled Pokemon\n \nWhat do you decide to do?\n \n1. Train in Viridian Forest\n \n2. It doesn't matter, let's just go face Brock";
+            String text10 = "Brock, the Gym Leader of Pewter City, has really strong and high-leveled Pokemon\n \nLets go fight him!";
 
             tvStoryText.setText(text10);
 
-            btn1.setText("Level Pokemon");
-            btn2.setText("Battle Brock");
+            btn1.setText("Battle Brock");
 
             btn1.setVisibility(View.VISIBLE);
-            btn2.setVisibility(View.VISIBLE);
 
             btn1.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    continueToLevelUpPokemon();
-                }
-            });
-
-            btn2.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
                     continueToBrock();
                 }
             });
+
         }
-
-    public void continueToLevelUpPokemon()
-    {
-        String text34 = "You've decided to level up " + playerPokemon.getName() + "\n \nGo and level up!";
-        tvStoryText.setText(text34);
-
-        btn1.setText("Continue");
-        btn2.setVisibility(View.INVISIBLE);
-        btn3.setVisibility(View.INVISIBLE);
-        btn1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                levelPokemon();
-            }
-        });
-    }
 
         public void continueToBrock()
         {
@@ -222,250 +198,11 @@ public class GameSannAaron extends GameActivity {
             btn3.setVisibility(View.INVISIBLE);
             btn1.setOnClickListener(new View.OnClickListener() {
                 @Override
-                public void onClick(View view) {
-                    SannPokemon brocksOnix = SannPokemonFactory.onix();
-                    initiateBattle(playerPokemon, brocksOnix);
-                }
-            });
-        }
-
-        public void initiateBattle(SannPokemon playerPokemon, SannPokemon wildPokemon) {
-
-            String text14 = "Get ready to battle!\n \nYou sent out " + playerPokemon.getName() + "!";
-            tvStoryText.setText(text14);
-
-            btn1.setText("Continue");
-            btn2.setVisibility(View.INVISIBLE);
-            btn3.setVisibility(View.INVISIBLE);
-            btn1.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    boolean battleOver = false;
-                    runBattle(wildPokemon, battleOver);
-                }
-            });
-
-        }
-
-        public void runBattle(SannPokemon wildPokemon, boolean battleOver) {
-
-            if (playerPokemon.getHealth() <= 0) {
-                youFainted();
-            }
-            if (wildPokemon.getHealth() <= 0) {
-                wildPokemonFainted(wildPokemon);
-            }
-            setContentView(R.layout.activity_sannaaron_battle);
-
-            TextView tvP1HealthVal = findViewById(R.id.tv_p1_health_val);
-            TextView tvP2HealthVal = findViewById(R.id.tv_p2_health_val);
-
-            tvP1HealthVal.setText("" + playerPokemon.getHealth());
-            tvP2HealthVal.setText("" + wildPokemon.getHealth());
-
-            btn1.setVisibility(View.VISIBLE);
-            btn2.setVisibility(View.VISIBLE);
-
-            btn1.setText("Attack");
-            btn2.setText("Run");
-
-            btn1.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-
-                }
-            });
-
-            btn2.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-
-                }
-            });
-
-
-            // Player's turn
-            String text15 = "What will " + playerPokemon.getName() + " do?";
-            String text16 = "1. Attack";
-            String text17 = "2. Run";
-            tvStoryText.setText(text15);
-            tvStoryText.setText(text16);
-            tvStoryText.setText(text17);
-
-            btn1.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    int playerDamage = playerPokemon.calculateDamage(wildPokemon);
-                    wildPokemon.takeDamage(playerDamage);
-
-                    String text18 = playerPokemon.getName() + " dealt " + playerDamage + " damage to " + wildPokemon.getName();
-                    tvStoryText.setText(text18);
-
-                    setAllBtnsVisible();
-                    btn1.setText("Continue");
-                    btn2.setVisibility(View.INVISIBLE);
-                    btn3.setVisibility(View.INVISIBLE);
-
-                    btn1.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View view) {
-
-                            otherPokemonAttacksBack(wildPokemon);
-                        }
-                    });
-                }
-            });
-
-            btn2.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    String text19 = "You successfully ran away!";
-                    tvStoryText.setText(text19);
-                    boolean battleOver = true;
-
-                    setAllBtnsVisible();
-                    btn1.setText("Continue");
-                    btn2.setVisibility(View.INVISIBLE);
-                    btn3.setVisibility(View.INVISIBLE);
-
-                    btn1.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View view) {
-                            viridianForest();
-                        }
-                    });
-                }
-            });
-
-        }
-
-        private void otherPokemonAttacksBack(SannPokemon wildPokemon){
-            if(playerPokemon.getHealth() <= 0 || wildPokemon.getHealth() <= 0)
-            {
-                youFainted();
-            }
-
-            int wildDamage = wildPokemon.calculateDamage(playerPokemon);
-            playerPokemon.takeDamage(wildDamage);
-            String text20 = wildPokemon.getName() + " dealt " + wildDamage + " damage to " + playerPokemon.getName() + "\n \n" + playerPokemon.getName() + " HP: " + playerPokemon.getHealth() + "\n \n" + wildPokemon.getName() + " HP: " + wildPokemon.getHealth();
-            tvStoryText.setText(text20);
-
-            if (playerPokemon.getHealth() <= 0)
-            {
-                youFainted();
-
-            }
-
-
-        }
-
-        private void youFainted() {
-            String text23 = playerPokemon.getName() + " fainted. You ran to the nearest Pokemon Center to heal your Pokémon.";
-            tvStoryText.setText(text23);
-
-            btn1.setText("Continue");
-            btn2.setVisibility(View.INVISIBLE);
-            btn3.setVisibility(View.INVISIBLE);
-            btn1.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    viridianForest();
-                }
-            });
-
-        }
-
-        private void wildPokemonFainted(SannPokemon wildPokemon) {
-
-            String text24 = "You defeated the " + wildPokemon.getName() + "!";
-            tvStoryText.setText(text24);
-        }
-
-
-        /**
-         * Allows the player to level up their Pokemon by battling wild Pokemon.
-         * <br> Preconditions: The player has chosen to train in Viridian Forest.
-         * <br> Postconditions: The player's Pokemon may level up, and their stats may improve.
-         */
-        public void levelPokemon()
-        {
-            String text25 = "You're in Viridian Forest and find a wild Caterpie.";
-            tvStoryText.setText(text25);
-
-            boolean continueLeveling = true;
-            boolean shouldLevelUp = false;
-
-            while (continueLeveling) {
-                SannPokemon wildCaterpie = SannPokemonFactory.caterpie();
-                SannPokemon wildPokemon = SannPokemonFactory.caterpie();
-
-                String text26 = "A " + wildPokemon.getName() + " appeared!";
-                tvStoryText.setText(text26);
-
-                initiateBattle(playerPokemon, wildPokemon);
-
-                if (shouldLevelUp)
-                {
-                    playerPokemon.levelUp();
-                    String text27 = playerPokemon.getName() + " leveled up!";
-                    tvStoryText.setText(text27);
-                    String text28 = "Current stats: ";
-                    tvStoryText.setText(text28);
-                    String text29 = "Pokemon:\t" + playerPokemon.getName() + "\n" + "Level:\t" + playerPokemon.getLevel() + "\n" + "Health:\t" + playerPokemon.getHealth() + "\n" + "Attack:\t" + playerPokemon.getAttack() + "\n" + "Defense:\t" + playerPokemon.getDefense();
-                    tvStoryText.setText(text29);
-                }
-
-                String text30 = "Do you want to train more? (1. Yes / 2. No)";
-                tvStoryText.setText(text30);
-
-                btn1.setText("Yes");
-                btn2.setText("No");
-
-                btn1.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        boolean continueLeveling = false;
-                        boolean shouldLevelUp = false;
-                    }
-                });
-
-                btn2.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        boolean shouldLevelUp = true;
-                    }
-                });
-
-            }
-
-            String text31 = "What do you decide to do now?";
-            String text32 = "1. Continue leveling up";
-            String text33 = "2. Go face Brock";
-            tvStoryText.setText(text31);
-            tvStoryText.setText(text32);
-            tvStoryText.setText(text33);
-
-
-
-            btn1.setText("Continue leveling up");
-            btn2.setText("Go face Brock");
-
-            btn1.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    levelPokemon();
-                }
-            });
-
-            btn2.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
+                public void onClick(View view) {;
                     pewterCityGym();
                 }
             });
         }
-
-
 
         /**
          * Initiates a battle against Brock, the Pewter City Gym Leader.
@@ -482,14 +219,8 @@ public class GameSannAaron extends GameActivity {
          */
         public void pewterCityGym() {
 
-            String text40 = "You enter the Pewter City Gym, and Brock is there waiting.\n";
-            String text41 = "He says, 'Are you sure you want to face me right now?";
-            String text42 = "1. Yes";
-            String text43 = "2. No";
+            String text40 = "You enter the Pewter City Gym, and Brock is there waiting.\n \nHe says, 'Are you sure you want to face me right now?\n \n1. Yes\n \n2. No";
             tvStoryText.setText(text40);
-            tvStoryText.setText(text41);
-            tvStoryText.setText(text42);
-            tvStoryText.setText(text43);
 
             btn1.setText("Yes");
             btn2.setText("No");
@@ -497,8 +228,7 @@ public class GameSannAaron extends GameActivity {
             btn1.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    SannPokemon brocksOnix = SannPokemonFactory.onix();
-                    initiateBattle(playerPokemon, brocksOnix);
+                    battleWithBrock();
                 }
             });
 
@@ -509,6 +239,67 @@ public class GameSannAaron extends GameActivity {
                 }
             });
         }
+
+        public void battleWithBrock()
+        {
+            btn1.setText("Continue");
+            btn2.setVisibility(View.INVISIBLE);
+            btn3.setVisibility(View.INVISIBLE);
+            btn1.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {;
+                    pewterCityGym();
+                }
+            });
+
+            String text1 = "1. Attack\n \n2. Forfeit";
+            tvStoryText.setText(text1);
+
+            btn1.setText("Yes");
+            btn2.setText("No");
+
+            btn1.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    brockLoses();
+                }
+            });
+
+            btn2.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    youForfeit();
+                }
+            });
+        }
+
+        public void brockLoses(){
+            btn1.setText("Continue");
+            btn2.setVisibility(View.INVISIBLE);
+            btn3.setVisibility(View.INVISIBLE);
+            btn1.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {;
+                    pewterCityGym();
+                }
+            });
+            String text1 = "Congrats you Won against Brock, so start getting ready for the next gym!";
+            tvStoryText.setText(text1);
+        }
+
+    public void youForfeit(){
+        btn1.setText("Continue");
+        btn2.setVisibility(View.INVISIBLE);
+        btn3.setVisibility(View.INVISIBLE);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {;
+                pewterCityGym();
+            }
+        });
+        String text1 = "Oh no! You forfeited the battle against Brock. You should try facing him again";
+        tvStoryText.setText(text1);
+    }
 
 
         /**
